8a02931745027b4ee670a5cd193d9d702a030eaf
Māris Ūdris <maris.udris.lv@gmail.com>
2022-01-12 21:33:51 +0200

    Add form validation

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 'use strict';
 const form = document.querySelector('.js-form');
-const emailField = form.email;
+const email = form.email;
+const warning = document.querySelector('.js-warning');
+
+function checkSubmit(evt) {
+    if (isValidEmail(email.value)) {
+        return;
+    }
+    evt.preventDefault();
+    email.setAttribute('aria-invalid', 'true');
+    warning.hidden = false;
+    email.addEventListener(
+        'input',
+        () => {
+            email.removeAttribute('aria-invalid');
+            warning.hidden = true;
+        },
+        { once: true }
+    );
+}
+
+function isValidEmail(email) {
+    const emailRegex =
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return emailRegex.test(email);
+}
+
+form.addEventListener('submit', checkSubmit);
